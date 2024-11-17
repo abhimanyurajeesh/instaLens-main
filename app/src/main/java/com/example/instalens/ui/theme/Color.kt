66b4b7258dcde12c523454a3a6f1aeeb87d07310
abhimanyurajeesh <abhimanyurajeesh@gmail.com>
2024-11-17 18:03:03 +0530

    first commit

---
+++ app/src/main/java/com/example/instalens/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.example.instalens.ui.theme
+
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.ui.graphics.Color
+
+// Colors for Dark Mode
+val Purple80 = Color(0xFFFF8A80)     // Black (Dark theme primary color)
+val PurpleGrey80 = Color(0xFFBDBDBD) // Light Gray for subtle contrast
+val Pink80 = Color(0xFFFFCDD2)       // A soft pastel pink
+
+
+// Colors for Light Mode
+val Purple40 = Color(0xFF888888)     // Deep Black (for a subtle shade)
+val PurpleGrey40 = Color(0xFF757575) // Medium Gray for a darker feel
+val Pink40 = Color(0xFFFF8A80)       // A light but slightly vibrant pink
+
